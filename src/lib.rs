4b0f66037a6e40b5dc7c8e12d22a7db056270904
JasonZhou89 <oncwnuBstCfa-7WDLYF6yB3OiY74@git.weixin.qq.com>
2022-04-10 22:02:53 +0800

    处理环境变量

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,7 +16,14 @@ impl Config {
         //这里clone可以进行优化
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        //读取环境变量CASE_INSENSITIVE，如果CASE_INSENSITIVE被这是为任何值，
+        //is_err会返回false
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -26,19 +35,35 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     //不同于panic!,? 会从函数中返回错误值并让调用者处理
     let contents = fs::read_to_string(config.filename)?;
-    
-    for line in search(&config.query, &contents){
-        println!("{}",line);
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
+        println!("{}", line);
     }
 
     Ok(())
 }
 
-pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     // vec![]
     let mut results = Vec::new();
-    for line in contents.lines(){
-        if line.contains(query){
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -49,12 +74,28 @@ pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
 mod tests {
     use super::*;
     #[test]
-    fn one_result(){
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
-        assert_eq!(vec!["safe, fast, productive."],search(query,contents));
+Pick three.
+Duct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
