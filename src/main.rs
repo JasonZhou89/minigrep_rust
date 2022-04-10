48b158f6b8e9974de8477048de614846185bf765
JasonZhou89 <oncwnuBstCfa-7WDLYF6yB3OiY74@git.weixin.qq.com>
2022-04-10 22:08:03 +0800

    将错误信息输出到标准错误而不是标准输出

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     // let (query,filename) = parse_config(&args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments:{}", err);
+        eprintln!("problem parsing arguments:{}", err);
         process::exit(1);
     });
     println!("searching for {} ", config.query);
@@ -17,7 +17,7 @@ fn main() {
 
     //这里没有使用unwrap_or_else，因为这里只关心错误情况，正确只会返回()没有实际意义
     if let Err(e) = minigrep::run(config) {
-        println!("Application error:{}", e);
+        eprintln!("Application error:{}", e);
         process::exit(1);
     }
 }
