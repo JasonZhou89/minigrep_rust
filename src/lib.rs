dc6889409e5c4c9c60dfaa8cd41e92eced65a257
JasonZhou89 <oncwnuBstCfa-7WDLYF6yB3OiY74@git.weixin.qq.com>
2022-04-10 21:23:38 +0800

    测试驱动开发

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,35 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     //不同于panic!,? 会从函数中返回错误值并让调用者处理
     let contents = fs::read_to_string(config.filename)?;
-    println!("with text:\n{}", contents);
+    
+    for line in search(&config.query, &contents){
+        println!("{}",line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query:&str,contents:&'a str)->Vec<&'a str>{
+    // vec![]
+    let mut results = Vec::new();
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."],search(query,contents));
+    }
 }
